fdded78422a3a927f9ad787660e98d9359027542
RoniYehoshua <roni6531607@gmail.com>
2024-04-24 08:55:37 +0300

    .

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,26 +7,25 @@ const logoStyle = {
   flexDirection: 'column',
   alignItems: 'center',
   width: 'fit-content',
-  margin: '0 auto',
-  // padding: '40px',
-  backgroundImage:'radial-gradient( #8C6849 0%, #8C6849 23%,  rgba(140, 104, 73, 0) 100%)'
-  
+  margin: '0',
+  //padding: '40px',
+  //backgroundImage:'radial-gradient( #8C6849 0%, #8C6849 23%,  rgba(140, 104, 73, 0) 100%)'
 }
 
 export default function Logo() {
 
   return (
-    <div style={logoStyle}>
-      <img src={LogoImg} width={'40%'} alt="לוגו המלונה" />
+    <div style={{logoStyle}}>
+      <img src={LogoImg} width={'50%'} alt="לוגו המלונה" />
     </div>
   )
 }
 
 Logo.Full = () => {
   return (
-    <div style={{...logoStyle,}}>
-      <img src={LogoImg} width={'40%'} alt="לוגו המלונה" />
-      <img src={Slogen} width={'70%'} style={{ maxWidth: '300px' }} alt="לוגו המלונה" />
+    <div style={{...logoStyle}}>
+      <img src={LogoImg} style={{width:'50%'}} alt="לוגו המלונה" />
+      <img src={Slogen} width={'100%'} style={{ maxWidth: '300px'}} alt="לוגו המלונה" />
     </div>
   )
 }
--- src/pages/LoginSystem/index.jsx
+++ src/pages/LoginSystem/index.jsx
@@ -4,6 +4,11 @@ import Logo from '../../components/Logo'
 
 import { useMediaQuery } from 'react-responsive'
 import { useTheme } from '@mui/material/styles'
+import { TextField } from '@mui/material'
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
 
 const phoneStyle = {
   width: '90%',
@@ -16,15 +21,30 @@ export default function LogInPage() {
 
   const isDesktop = useMediaQuery({ query: '(min-width:992px )' })
 
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+
   const theme = useTheme();
 
   return (
-    <BackgroundLayout image={LogInImage}>
-      <div style={{padding:'50px 0 ', ...isDesktop ? desktopStyle : phoneStyle  }}>
-        <Logo.Full />
-        <h3>לנהל את הכלבייה שלך בצורה הפשוטה ביותר.</h3>
+    <>
+      <BackgroundLayout image={LogInImage}>
+        <div style={{ padding: '50px 0', backgroundImage: 'radial-gradient( #8C6849 0%, #8C6849 23%,  rgba(140, 104, 73, 0) 100%)', ...isDesktop ? desktopStyle : phoneStyle }}>
+          <Logo.Full />
+          <h3 style={{ color: 'white', marginTop: '5px' }}>לנהל את הכלבייה שלך בצורה הפשוטה ביותר.</h3>
+          <CacheProvider value={cacheRtl}>
+            <div dir='rtl'>
+              <TextField id="filled-basic" label="Outlined" variant="filled" style={{ backgroundColor: 'rgb(255,255,255,0.7)', borderRadius: '10px' }} /> <br/><br/>
+              <TextField id="filled-basic" label="אימייל או שם משתמש" variant="filled" required style={{ backgroundColor: 'rgb(255,255,255,0.7)', borderRadius: '10px 10px 0px 0px' }} /> <br /> <br />
+              <TextField id="filled-basic" label="סיסמה" variant="filled" required style={{ backgroundColor: 'rgb(255,255,255,0.7)', borderRadius: '10px 10px 0px 0px' }} />
+            </div>
+          </CacheProvider>
+        </div>
+      </BackgroundLayout>
+      <br/><br/><TextField id="outlined-basic" label="Outlined" variant="outlined" sx={{borderColor:'red'}} />
 
-      </div>
-    </BackgroundLayout>
+    </>
   )
 }
