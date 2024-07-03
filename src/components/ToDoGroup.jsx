b7c015dde56d2c448647295350e13c2529dd5f12
Ben Shuan <benshuan@gmail.com>
2024-07-03 18:16:52 +0300

    /

--- src/components/ToDoGroup.jsx
+++ src/components/ToDoGroup.jsx
@@ -1,4 +1,4 @@
-import { Box, List, Paper, Typography } from '@mui/material'
+import { Backdrop, Box, Button, Divider, Fade, List, Modal, Paper, Slide, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import { ToDoList } from '../modules/Admin/components/ToDoList'
 import { ToDoItem } from './ToDoItem'
@@ -7,6 +7,7 @@ import useFetch from '../utilis/useFetch'
 import { Check } from '@mui/icons-material'
 import { DndContext } from '@dnd-kit/core'
 import { SortableContext } from '@dnd-kit/sortable'
+import dayjs from 'dayjs'
 
 export const ToDoGroup = ({ title, date }) => {
 
@@ -16,7 +17,7 @@ export const ToDoGroup = ({ title, date }) => {
 
   const todos = useFetch(import.meta.env.VITE_APP_SERVERURL + `ToDos/shelter/${loginDet.shelterNumber}/date/${date}`, {}, [trigger])
 
-  
+
 
   return (
 
@@ -40,4 +41,90 @@ export const ToDoGroup = ({ title, date }) => {
       }
     </Box>
   )
+}
+
+
+ToDoGroup.Calender = ({ date }) => {
+  const { loginDet } = useShelterContext();
+
+  const [modalData, setModalData] = useState(null)
+
+  const todos = useFetch(import.meta.env.VITE_APP_SERVERURL + `ToDos/shelter/${loginDet.shelterNumber}/date/${date}`)
+
+  let numOnButton = todos.value?.length - 2
+
+  const renderToDos = () => {
+
+    return todos.value?.map((item, i) => {
+      if (i < 2) {
+
+        return <>
+          <Typography sx={{ fontSize: '12px', whiteSpace: 'collapse' }} noWrap key={item.toDoId}>{item.title}</Typography>
+
+        </>
+      }
+    })
+  }
+
+  const openModal = () => {
+
+    let title = dayjs(date).locale('he').format('dddd, D בMMMM');
+
+    setModalData({ title, date })
+
+  }
+
+
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, cursor: 'pointer' }} onClick={openModal} >
+        <Box>
+
+          {renderToDos()}
+        </Box>
+        {
+          todos.value?.length > 2 &&
+          <Button color='primary' > {numOnButton}+ </Button>
+        }
+
+      </Box>
+      <Modal open={modalData !== null}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+        onClose={() => setModalData(null)}
+        sx={{
+          display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+          "& .MuiBackdrop-root": {
+            backgroundColor: 'rgba(255,255,255,0.3)'
+          },
+        }}>
+        <Fade in={modalData!==null} >
+
+          <Box sx={{
+            padding: '20px', borderRadius: '20px', bgcolor: 'secondary.main',
+            border: '2px solid', borderColor: 'primary.main', position: 'relative',
+            height: '75vh', width: 'clamp(200px,30vw,500px)',
+            "&:after": {
+              content: '""',
+              height: '100%',
+              position: 'absolute',
+              backgroundColor: 'white',
+              top: 0, right: 0, width: '100%',
+              borderRadius: 'inherit', zIndex: -1
+            }
+          }}>
+
+            <ToDoGroup date={modalData?.date} title={modalData?.title} />
+          </Box>
+        </Fade>
+      </Modal>
+    </>
+  )
+
 }
--- src/modules/Admin/components/ToDoCalender.jsx
+++ src/modules/Admin/components/ToDoCalender.jsx
@@ -7,135 +7,176 @@ import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
 import { useEffect, useRef, useState } from 'react';
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import { Box, Modal } from '@mui/material';
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import { ToDoGroup } from '../../../components/ToDoGroup';
 
-function getRandomNumber(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-/**
- * Mimic fetch with abort controller https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
- * ⚠️ No IE11 support
- */
-function fakeFetch(date, { signal }) {
-  return new Promise((resolve, reject) => {
-    const timeout = setTimeout(() => {
-      const daysInMonth = date.daysInMonth();
-      const daysToHighlight = [1, 2, 3].map(() => getRandomNumber(1, daysInMonth));
-
-      resolve({ daysToHighlight });
-    }, 500);
-
-    signal.onabort = () => {
-      clearTimeout(timeout);
-      reject(new DOMException('aborted', 'AbortError'));
-    };
-  });
-}
 
 const initialValue = dayjs();
 
 function ServerDay(props) {
-  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+  const { countTodos = [], day, outsideCurrentMonth, ...other } = props;
 
-  const isSelected =
-    !props.outsideCurrentMonth && highlightedDays.indexOf(props.day.date()) >= 0;
+  const count = countTodos.filter(item => dayjs(item.dayInMonth).diff(day, 'd') == 0)[0];
 
   return (
-    <Badge
+    <Box
       key={props.day.toString()}
-      overlap="rectangular"
-      badgeContent={isSelected ? '🌚' : undefined}
-      sx={{flexShrink:'none',flexGrow:'1',height:'20%'}}
-    
+      sx={{
+        padding: '5px',
+        width: 'calc(100% / 7)',
+        borderRadius: '10px',
+        display: 'flex',
+        alignItems: 'baseline',
+        overflow: 'auto',
+        gap: '12px',
+        "&:hover": {
+          bgcolor: 'primary.light',
+        }
+      }}
     >
-      <PickersDay  sx={{margin:'auto',"&.MuiPickersDay-root.Mui-selected":{bgcolor:theme=>theme.palette.secondary.dark , color:'black'}}} {...other} outsideCurrentMonth={outsideCurrentMonth} day={day}  />
-    </Badge>
+
+      <Badge
+        overlap="circular"
+        badgeContent={count?.countItems}
+        color='primary'
+      >
+        <PickersDay
+          sx={{
+            borderRadius: '5px',
+            "&.MuiPickersDay-root.Mui-selected": { bgcolor: theme => theme.palette.secondary.dark, color: 'black' }
+          }}
+          {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} >
+
+        </PickersDay>
+      </Badge>
+      <ToDoGroup.Calender date={day.locale('he').toISOString()} />
+    </Box>
   );
 }
 
 export default function TodoCalendar() {
-  const requestAbortController = useRef(null);
+
+  const months = [
+    "ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני", "יולי",
+    "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"
+  ]
+
+  const weekdays = ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת']
+
+
+
+  dayjs.extend(utc)
+  dayjs.extend(timezone)
+  dayjs.locale('he', {
+    months,
+    weekdays
+  })
+
   const [isLoading, setIsLoading] = useState(false);
-  const [highlightedDays, setHighlightedDays] = useState([1, 2, 15]);
+  const [countTodos, setCountToDos] = useState([]);
 
-  const fetchHighlightedDays = (date) => {
-    const controller = new AbortController();
-    fakeFetch(date, {
-      signal: controller.signal,
-    })
-      .then(({ daysToHighlight }) => {
-        setHighlightedDays(daysToHighlight);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        // ignore the error if it's caused by `controller.abort`
-        if (error.name !== 'AbortError') {
-          throw error;
+  const fetchHighlightedDays = () => {
+    fetch(import.meta.env.VITE_APP_SERVERURL + 'ToDos/counts/shelter/1/date/2024-07-07')
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
         }
-      });
-
-    requestAbortController.current = controller;
+        else {
+          return Promise.reject()
+        }
+      }
+      )
+      .then((data) => {
+        setIsLoading(false)
+        setCountToDos(data)
+      })
   };
 
   useEffect(() => {
     fetchHighlightedDays(initialValue);
-    // abort request on unmount
-    return () => requestAbortController.current?.abort();
+
   }, []);
 
+
+
   const handleMonthChange = (date) => {
-    if (requestAbortController.current) {
-      // make sure that you are aborting useless requests
-      // because it is possible to switch between months pretty quickly
-      requestAbortController.current.abort();
-    }
 
     setIsLoading(true);
-    setHighlightedDays([]);
+    setCountToDos([]);
     fetchHighlightedDays(date);
   };
 
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='he' >
       <DateCalendar
-        sx={{ width: '90%',height:'80vh',border:'3px solid',maxHeight:'none',
-          borderRadius:'15px',bottom:'5px',marginBottom:'5vh',
-          borderColor:theme=>theme.palette.primary.main }}
+        sx={{
+          width: '90%', height: '80vh', border: '3px solid', maxHeight: 'none',
+          borderRadius: '15px', bottom: '5px', marginBottom: '5vh',
+          borderColor: theme => theme.palette.primary.main,
+          ['& .MuiDayCalendar-weekDayLabel']: {
+            width: '100%',
+            fontSize: '16px',
+            fontWeight: 'bold'
+          },
+          "& .MuiDayCalendar-root ": {
+            height: 'calc(75vh - 40px)',
+            display: 'flex',
+            flexDirection: 'column',
+            "& .MuiPickersSlideTransition-root": {
+              flexGrow: 1,
+              "& .MuiDayCalendar-monthContainer": {
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '3px',
+                padding: '0 3px',
+                "& .MuiDayCalendar-weekContainer": {
+                  height: '100%',
+                  gap: '3px',
+                  margin: 0,
+                  maxHeight: 'calc(100% / 5)'
+
+                }
+              }
+            }
+
+          }
+
+
+        }}
+
+        dayOfWeekFormatter={(date) => date.locale('he').format('dddd')}
+        showDaysOutsideCurrentMonth
         defaultValue={initialValue}
         loading={isLoading}
-        views={['year','month','day']}
+        views={['year', 'month', 'day']}
         onMonthChange={handleMonthChange}
         renderLoading={() => <DayCalendarSkeleton />}
         slots={{
           leftArrowIcon: () => <KeyboardArrowRight />,
           rightArrowIcon: () => <KeyboardArrowLeft />,
           day: ServerDay,
-
         }}
 
         slotProps={{
-          layout: {
-            sx: {
-              ['.MuiMultiSectionDigitalClock-root']: {
-                flexDirection: 'row-reverse'
-              },
-              
-            }
-          }
-          ,
           day: {
-            highlightedDays,
-            
+            countTodos,
           },
-          calendarHeader:{
-            sx:{
-              ['.MuiDayCalendar-header']:{
-                justifyContent: 'space-around'
-              }
+          calendarHeader: {
+            sx: {
+              bgcolor: 'secondary.main',
+              mt: 0,
+
             }
           }
+
         }}
       />
+
+     
     </LocalizationProvider>
   );
 }
